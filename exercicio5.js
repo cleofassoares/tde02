f5bef4c4b3d893be41250a937af7427b6d6ed9f8
cleofassoares <113731308+cleofassoares@users.noreply.github.com>
2024-04-23 20:51:05 -0300

    Add files via upload

---
+++ exercicio5.js
@@ -0,0 +1,5 @@
+/*Use um loop for para exibir a tabuada do 7 no console (de 1 a 10).*/
+
+for(let i = 1; i <= 10; i++){
+    console.log (i * 7)
+}
---
+++ exercicio8.js
@@ -0,0 +1,12 @@
+/*Crie uma função chamada menorNumero que recebe três números como parâmetros e retorna o menor deles.*/
+function menorNumero(a,b,c){
+    if(a < b || a < c){
+        return a
+    }else if (b < a || b < c){
+        return b
+    }else{
+        return c
+    }
+}
+console.log(menorNumero(a = 1, b = 2, c = 3))
+
